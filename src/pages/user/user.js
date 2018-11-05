80abe7e00e5b7c5c355365f697a411db85c92189
liyan0411 <541267924@qq.com>
2018-11-05 16:19:18 +0800

    提交我的

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -5,7 +5,99 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    headImg: "http://47.93.13.54:333/zhangjiu/images/ico_touxiang@2x.png",
+    ewmImg: "http://47.93.13.54:333/zhangjiu/images/ico_erweima@2x.png",
+    userName:"杨先生",
+    isLogin:false,
+    cash:0,//现金积分
+    ordinary:0,//普通积分
+    icon1: "http://47.93.13.54:333/zhangjiu/images/ico_wodetuandui_dis@2x.png",
+    icon2: "http://47.93.13.54:333/zhangjiu/images/ico_wodedingdan_dis@2x.png",
+    icon3: "http://47.93.13.54:333/zhangjiu/images/Group 7@2x.png",
+  },
+  jumpLogin() {
+    console.log("去登录")
+    // wx.navigateTo({
+    //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+    // })
+  },
+  jumpMa(){
+    if (!this.isLogin){
+      wx.showToast({
+        title:"请登录",
+        icon:"none",
+        duration:2000
+      })
+    }else{
+       // wx.navigateTo({
+      //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+      // })
+    }
+  },
+  jumpOrdinary(){
+    if (!this.isLogin) {
+      wx.showToast({
+        title: "请登录",
+        icon: "none",
+        duration: 2000
+      })
+    } else {
+      // wx.navigateTo({
+      //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+      // })
+    }
+  },
+  jumpCash(){
+    if (!this.isLogin) {
+      wx.showToast({
+        title: "请登录",
+        icon: "none",
+        duration: 2000
+      })
+    } else {
+      // wx.navigateTo({
+      //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+      // })
+    }
+  },
+  myTeam(){
+    if (!this.isLogin) {
+      wx.showToast({
+        title: "请登录",
+        icon: "none",
+        duration: 2000
+      })
+    } else {
+      // wx.navigateTo({
+      //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+      // })
+    }
+  },
+  myDan() {
+    if (!this.isLogin) {
+      wx.showToast({
+        title: "请登录",
+        icon: "none",
+        duration: 2000
+      })
+    } else {
+      // wx.navigateTo({
+      //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+      // })
+    }
+  },
+  userAcc() {
+    if (!this.isLogin) {
+      wx.showToast({
+        title: "请登录",
+        icon: "none",
+        duration: 2000
+      })
+    } else {
+      // wx.navigateTo({
+      //   // url: '/pages/index/cargoApplyFor/cargoApplyFor',
+      // })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
