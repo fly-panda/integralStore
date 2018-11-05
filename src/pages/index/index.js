da9d16139f3573e0bd9f1121098242c65e65bcfc
韩德祥 <handexiang@hdxdeMacBook-Pro.local>
2018-11-05 14:10:30 +0800

    indx

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,47 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const config = require('../../utils/config.js');
 
 Page({
   data: {
-    videoSrc: '/'
+    videoSrc: '/',
+    imgUr: config.imgUrl,
+    vipData: [
+      {
+        imgUrl: config.imgUrl + 'images/ico_vip_dis@2x.png',
+        title: 'VIP',
+        color: '#F26132',
+        tip: '价值1299的红酒一瓶\n获得20%的销售返利',
+        prize: '399'
+      },
+      {
+        imgUrl: config.imgUrl + 'images/ico_tianshihehuoren_dis@2x.png',
+        title: '天使合伙人',
+        color: '#118BD9',
+        tip: '价值2299的红酒一瓶\n获得20%的销售返利',
+        prize: '1300'
+      },
+      {
+        imgUrl: config.imgUrl + 'images/ico_gaojihehuoren_dis@2x.png',
+        title: '高级合伙人',
+        color: '#B7905A',
+        tip: '价值3299的红酒一瓶\n获得全部销售的20%返利',
+        prize: '3900'
+      },
+      {
+        imgUrl: config.imgUrl + 'images/ico_shiyehehuoren_dis@2x.png',
+        title: '事业合伙人',
+        color: '#06865D',
+        tip: '价值3299的红酒一瓶\n获得全部销售的20%返利',
+        prize: '3900'
+      }
+    ]
+  },
+  commodity() {
+    wx.navigateTo({
+      url: '/pages/index/commodity/commodity',
+    })
   },
   onLoad: function () {
 
