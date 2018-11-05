f6680b0da494f473cb4770c000b9a211bcfa584f
fly-panda <893991581@qq.com>
2018-11-05 20:45:19 +0800

    登录&注册

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,108 @@
 // pages/login/login.js
+const app = getApp();
+const config = require('../../utils/config.js');
+const rgexpPhone = /^1\d{10}$/
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
+  maxTime: 60,
+  currentTime: 60,  // 倒计时时间（单位：秒）
+  interval: null,
+  phoneNum: '',
+  code: '',
   data: {
-
+    txtMsg: '获取验证码',
+    imgUrl: config.imgUrl,
+    submitDisabled: true,
+    codeStatus: true    
   },
+  // 获取验证码
+  getCode() {
+    if (this.data.codeStatus) {
+      if (!this.phoneNum) {
+        wx.showToast({
+          title: '手机号不能为空',
+          icon: 'none'
+        })
+        return
+      }
 
+      if (rgexpPhone.test(this.phoneNum) == false) {
+        wx.showToast({
+          title: '手机号格式不正确',
+          icon: 'none'
+        })
+        return
+      }
+
+
+
+      this.setData({
+        codeStatus: false,
+        txtMsg: this.currentTime + 's后重发'
+      });
+      this.interval = setInterval(() => {
+        if (this.currentTime > 1) {
+          this.currentTime--;
+          this.setData({
+            txtMsg: this.currentTime + 's后重发'
+          });
+        } else {
+          clearInterval(this.interval);
+          this.currentTime = this.maxTime;
+          this.setData({
+            codeStatus: true,
+            txtMsg: '免费获取'
+          });
+        }
+      }, 1000);
+    }
+  },
+  getPhoneNum(e) {
+    this.phoneNum = e.detail.value
+    let submitDisabled = this.phoneNum != '' && this.code != '' ? false : true
+    console.log(submitDisabled)
+    this.setData({
+      submitDisabled
+    })  
+  },
+  getCodeInput(e) {
+    this.code = e.detail.value    
+    let submitDisabled = this.phoneNum != '' && this.code != '' ? false : true
+    this.setData({
+      submitDisabled
+    })    
+  },
+  login(e) {
+    console.log(e)
+    let phoneNum = e.detail.value.phoneNum
+    let code = e.detail.value.code
+    if (!phoneNum) {
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none'
+      })
+      return
+    }
+    if (rgexpPhone.test(phoneNum) == false) {
+      wx.showToast({
+        title: '手机号格式不正确',
+        icon: 'none'
+      })
+      return
+    }
+    if (!code) {
+      wx.showToast({
+        title: '验证码不能为空',
+        icon: 'none'
+      })
+      return
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -35,7 +130,11 @@ Page({
   onHide: function () {
 
   },
-
+  signUp() {
+    wx.navigateTo({
+      url: '/pages/sign-up/sign-up',
+    })
+  },
   /**
    * 生命周期函数--监听页面卸载
    */
@@ -60,7 +159,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  // onShareAppMessage: function () {
 
-  }
+  // }
 })
