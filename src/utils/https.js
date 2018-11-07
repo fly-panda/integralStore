b497a16b35e993fbbc56d252eebdfdafb409f42f
韩德祥 <handexiang@hdxdeMacBook-Pro.local>
2018-11-07 17:03:39 +0800

    https

--- src/utils/https.js
+++ src/utils/https.js
@@ -1,24 +1,68 @@
-// ci环境接口
-// const url = 'https://ck-static.yun.pingan.com/static/ci/backend/esg'; 
-// 生产环境接口
-const url = 'https://ziker-static.yun.pingan.com/static/backend/esg';
+const apiURL = 'https://www.badazhou.com';
 
 const wxRequest = function (param) {
 
+  let url = '';
+
+  if (param.url.substr(0, 5) == 'https') {
+    url = param.url;
+
+  } else {
+    if (param.url.substr(0, 1) == '/') {
+      param.url = param.url.substr(1);
+    }
+
+    url = apiURL + '/' + param.url;
+  }
+
+
+
   if (!param.method) {
-    param.method = 'POST';
+    param.method = 'GET';
   }
 
   const method = param.method.toUpperCase();
+
+  let header = param.header || {};
+
+  // 总是把accessToken作为header头传给服务器
+  let accessToken = wx.getStorageSync('accessToken');
+
+  if (accessToken) {
+    header.Authorization = accessToken;
+  }
+
+
+
 
   wx.request({
     url: url,
     method: method,
     data: param.data || {},
+    header: header,
     success(res) {
-      if (param.success) {
-        param.success(res);
+      // 拦截一下末登录的请求，末登录，跳转到登录页
+      if (res.data.code == -1001) {
+
+        wx.navigateTo({
+          url: '/pages/member/login/login'
+        })
+
+      } else if (res.data.code == -1100) {
+        wx.showToast({
+          title: res.data.msg,
+          icon: 'none',
+          duration: 2000
+        })
+      } else {
+        if (param.success) {
+          param.success(res);
+        }
       }
+
+
+
+
     },
     fail(res) {
       wx.getNetworkType({
@@ -45,7 +89,15 @@ const wxRequest = function (param) {
   });
 }
 
+function isEmoji(string) {
+  var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+  return regex.test(string);
+
+}
+
+
 module.exports = {
-  wxRequest
+  wxRequest,
+  isEmoji
 };
 
