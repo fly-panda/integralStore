72148037ba5691991ea1e8c215c18f7f8885ecbe
liyan <liyan@anji-plus.com>
2018-11-13 11:13:43 +0800

    提交最新

--- src/pages/user/my-order/my-order.js
+++ src/pages/user/my-order/my-order.js
@@ -8,9 +8,9 @@ Page({
     bgImgUrl: config.imgUrl + "ico_kongzhuangtai_dis@2x.png",
     orderImg: config.imgUrl + "ico_tianshihehuoren_dis@2x.png",
     listData:[
-      {title:"天使合伙人",type:"微信支付+积分",status:"已发货",price:"1300",date:"2018/10/31"},
-      { title: "天使合伙人", type: "微信支付+积分", status: "已发货", price: "1300", date: "2018/10/31" },
-      { title: "天使合伙人", type: "微信支付+积分", status: "已发货", price: "1300", date: "2018/10/31" },
+      { title: "天使合伙人", type: "微信支付+积分", status: "已发货",kdname:"中通快递",kdnum:"283423423432034",price:"1300",date:"2018/10/31"},
+      { title: "天使合伙人", type: "微信支付+积分", status: "已发货", kdname: "", kdnum: "", price: "1300", date: "2018/10/31" },
+      { title: "天使合伙人", type: "微信支付+积分", status: "已发货", kdname: "中通快递", kdnum: "283423423432034", price: "1300", date: "2018/10/31" },
     ]
   },
   goto(){
