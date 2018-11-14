991d1aff5f3c2da02aa4149eb208dfeb571f74bb
韩德祥 <handexiang@hdxdeMacBook-Pro.local>
2018-11-14 16:03:05 +0800

    订单确认页面

--- src/pages/confirmation-oder/confirmation-oder.js
+++ src/pages/confirmation-oder/confirmation-oder.js
@@ -1,16 +1,19 @@
 // pages/confirmation-oder/confirmation-oder.js
 const app = getApp();
 const config = require('../../utils/config.js');
+const https = require('../../utils/https.js');
 
 Page({
 
   /**
    * 页面的初始数据
    */
+  clientbm: wx.getStorageSync("clientbm"),
   data: {
     imgUrl: config.imgUrl,
     payType: 0, // 支付方式 0-微信支付，1-现金支付
-    isUserJf: false 
+    isUserJf: false,
+    jcInfo: {}
   },
   selectPayType(e) {
     let type = e.currentTarget.dataset.type
@@ -28,6 +31,24 @@ Page({
   changeAddress() {
     wx.navigateTo({
       url: '/pages/user/user-set/change-address/change-address',
+    })
+  },
+  getAddress() {
+    https.wxRequest({
+      url: 'member_basic_info/',
+      data: { clientbm: this.clientbm},
+      success: res => {
+        console.log(res)
+        if (res.statusCode == '200') {
+          if (res.data.returnvalue == 'true') {
+            let jcInfo = res.data
+            jcInfo.shmobile = jcInfo.shmobile.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
+            this.setData({
+              jcInfo
+            })
+          }
+        }
+      }
     })
   },
   /**
@@ -48,7 +69,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getAddress()
   },
 
   /**
